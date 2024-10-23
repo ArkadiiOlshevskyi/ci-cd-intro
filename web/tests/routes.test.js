2059db54482af38c33c30bc319c22d887a0c1a32
Arkadii <ark.dev.code@gmail.com>
2024-10-23 17:32:51 +0100

    Update routes.test.js -> 205 to fail unit test

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 260,
+      count: 205,
       shape: 'circle'
     })
     expect(res.statusCode).toEqual(201)
